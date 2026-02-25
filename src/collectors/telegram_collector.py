4070c8f17a6145fd541bab02928b29a38c2fa829
nikolaj278 <76949219+nikolaj278@users.noreply.github.com>
2026-02-25 10:18:40 +0200

    Update telegram_collector.py
    Corrected "is not" to inequality operator in string comparison.

--- src/collectors/telegram_collector.py
+++ src/collectors/telegram_collector.py
@@ -48,7 +48,7 @@ class TelegramCollector:
                     )
                     for m in msgs
                     # skip empty or emoji only strings
-                    if (m.text is not None) and (replace_emoji(m.text, "") is not "")
+                    if (m.text is not None) and (replace_emoji(m.text, "") != "")
                 ]
                 # if there are new messages containing text
                 if msg_objects:
